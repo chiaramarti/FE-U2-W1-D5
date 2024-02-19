bfad208f3d59462d5e3670d99a3e10ab7665cdbc
chiaramarti <martinellichiara94@gmail.com>
2024-02-19 13:28:54 +0100

    M animation 1.0

--- base_di_partenza/assets/js/script.js
+++ base_di_partenza/assets/js/script.js
@@ -12,3 +12,30 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+// tentativi di animazione
+const pathElements = document.querySelectorAll("svg path");
+
+function showPathElements() {
+  pathElements.forEach((pathElement) => {
+    pathElement.style.opacity = 1;
+  });
+}
+
+function hideRandomPathElement() {
+  showPathElements();
+
+  for (i = 1; i < 20; i++) {
+    const randomIndex = Math.floor(Math.random() * pathElements.length);
+
+    pathElements[randomIndex].style.opacity = 0;
+    pathElements[randomIndex].style.transition = "500ms";
+  }
+}
+function animatePaths() {
+  hideRandomPathElement();
+  setTimeout(animatePaths, 500);
+}
+
+// Inizia l'animazione
+animatePaths();
